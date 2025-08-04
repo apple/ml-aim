bd9e06893f3ad68bda56b34ac2158fda2680b7fc
Michal Klein <46717574+michalk8@users.noreply.github.com>
2025-08-04 19:58:38 +0200

    Fix HF revision

--- aim-v2/aim/v2/utils.py
+++ aim-v2/aim/v2/utils.py
@@ -6,31 +6,31 @@ from huggingface_hub import hf_hub_download
 __all__ = ["load_pretrained"]
 
 
-MODELS = (
+MODELS = {
     # 224
-    "aimv2-large-patch14-224",
-    "aimv2-huge-patch14-224",
-    "aimv2-1B-patch14-224",
-    "aimv2-3B-patch14-224",
+    "aimv2-large-patch14-224": "ac764a25c832c7dc5e11871daa588e98e3cdbfb7",
+    "aimv2-huge-patch14-224": "9e0bab0e0abb7b20796d2d0b37f39b7c3b1c1af1",
+    "aimv2-1B-patch14-224": "4d24a422f71e87d327d476bc405030fe6593d8b1",
+    "aimv2-3B-patch14-224": "df1cbc288096c9742ec8bd6709ff82757972338c",
     # 336
-    "aimv2-large-patch14-336",
-    "aimv2-huge-patch14-336",
-    "aimv2-1B-patch14-336",
-    "aimv2-3B-patch14-336",
+    "aimv2-large-patch14-336": "639423ae9f07319d24a7fc431b61908110d08d3a",
+    "aimv2-huge-patch14-336": "21528bd53a2438a84162e79f1f9a46453b88004c",
+    "aimv2-1B-patch14-336": "5a15f23f6757776fdd487283353a018677621939",
+    "aimv2-3B-patch14-336": "d1adb39ee92dfd7ecf3114b1ee3aa7e9027ce98f",
     # 448
-    "aimv2-large-patch14-448",
-    "aimv2-huge-patch14-448",
-    "aimv2-1B-patch14-448",
-    "aimv2-3B-patch14-448",
+    "aimv2-large-patch14-448": "cefb13f21003bdadba65bfbee956c82b976cd23d",
+    "aimv2-huge-patch14-448": "44e6440a24a8ea966ad0deadf5399a22d852bcc0",
+    "aimv2-1B-patch14-448": "7f292735d3a07a911559c0fabb3ad3e9d141713f",
+    "aimv2-3B-patch14-448": "70810b618e4456b724bacc8ef4d2d038060ceda6",
     # distilled
-    "aimv2-large-patch14-224-distilled",
-    "aimv2-large-patch14-336-distilled",
+    "aimv2-large-patch14-224-distilled": "d6d09b071e8ba31735c5e05ccd4b2a393020ce90",
+    "aimv2-large-patch14-336-distilled": "8fe1704e5cc1c80cefc99c194afcefe21dff1faa",
     # native
-    "aimv2-large-patch14-native",
+    "aimv2-large-patch14-native": "cfe7bdbbc924a5f65272d5e2c27fe5f61771a629",
     # lit
-    "aimv2-large-patch14-224-lit",
-)
-ModelName = Literal[MODELS]
+    "aimv2-large-patch14-224-lit": "c2cd59a786c4c06f39d199c50d08cc2eab9f8605",
+}
+ModelName = Literal[tuple(MODELS)]
 
 
 def _get_weights_fname(backend: Literal["torch", "jax", "mlx"]) -> str:
@@ -114,10 +114,19 @@ def load_pretrained(
         Otherwise, returns the pre-trained model containing the weights.
     """
     if model_name not in MODELS:
-        raise ValueError(f"Invalid model: {model_name}. Valid models are: {MODELS}.")
+        raise ValueError(
+            f"Invalid model: {model_name}. Valid models are: {tuple(MODELS.keys())}."
+        )
 
     fname = _get_weights_fname(backend)
-    weights_path = hf_hub_download(f"apple/{model_name}", filename=fname, **kwargs)
+    revision = MODELS[model_name]
+    print(model_name, revision)
+    weights_path = hf_hub_download(
+        f"apple/{model_name}",
+        filename=fname,
+        revision=revision,
+        **kwargs,
+    )
 
     if backend == "torch":
         model = _load_torch(model_name, weights_path)
